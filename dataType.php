37e99dbddb603be484a9dbeaab925590a28ecb66
jishan1019 <imranislamjishan200@gmail.com>
2023-10-22 00:13:47 +0600

    data type practice

---
+++ dataType.php
@@ -0,0 +1,16 @@
+<?php
+
+$vagitable = 'Apple';
+$allVagitable = 'I love all type';
+
+$num = 15;
+
+$result = "The Result Is: ";
+
+echo $result.$num;
+
+echo $vagitable;
+
+echo  "$allVagitable  $vagitable";
+
+?>
