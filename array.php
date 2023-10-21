f3708c7d8c0963e3b2ae9b82340a9224354c0402
jishan1019 <imranislamjishan200@gmail.com>
2023-10-22 00:24:09 +0600

    array show with 3 method

--- array.php
+++ array.php
@@ -1,9 +1,17 @@
 <?php
     
     $arr = ['Rohim','Korim','Mofiz','Hafiz'];
+    $arr1 = array('Gazi','Razi','Mofiz','Hafiz');
+
+    //Array Custom index show
+    $arr2 = array('frist' => 'Sufi', 'second' => 'Rufi');
 
     // echo $arr[0]
 
     print_r($arr);
+    print_r($arr1)
+    print_r($arr2)
+
+
 
 ?>
